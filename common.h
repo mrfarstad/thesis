2d949cf125c2cfe4ac01e0879327388f5b521908
Martin Rebne Farstad <martinrf@stud.ntnu.no>
2020-10-25 09:44:55 +0100

    feat: Add timing tool

--- common.h
+++ common.h
@@ -1,3 +1,5 @@
+#include <sys/time.h>
+
 #ifndef _COMMON_H
 #define _COMMON_H
 
@@ -61,4 +63,13 @@
         exit(1);                                                               \
     }                                                                          \
 }
+
+inline double seconds()
+{
+    struct timeval tp;
+    struct timezone tzp;
+    int i = gettimeofday(&tp, &tzp);
+    return ((double)tp.tv_sec + (double)tp.tv_usec * 1.e-6);
+}
+
 #endif // _COMMON_H
