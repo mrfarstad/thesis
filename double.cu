971f431abfa776d983a286ebd5a9299682ecfe3c
Martin Rebne Farstad <martinrf@stud.ntnu.no>
2020-10-29 22:52:03 +0100

    This is not so bad WIP

--- double.cu
+++ double.cu
@@ -15,8 +15,10 @@ void test(int *g_u1)
 
 int main(int argc, char *argv[]) {
 
-
     int ngpus = 2;
+
+    cudaLaunchParams *launchParams = (cudaLaunchParams*) malloc(sizeof(cudaLaunchParams) * ngpus);
+    cudaStream_t     *streams      = (cudaStream_t*)     malloc(sizeof(cudaStream_t)     * ngpus);
 
     // set up gpu card
     int *d_u1[ngpus];
@@ -29,26 +31,65 @@ int main(int argc, char *argv[]) {
     for (int i = 0; i < ngpus; i++)
     {
         CHECK(cudaSetDevice(i));
+        CHECK(cudaStreamCreate(&streams[i]));
         CHECK(cudaMalloc((void **) &d_u1[i], ibyte));
     }
 
     dim3 block(isize);
     dim3 grid(1);
 
+
+    void *args[ngpus][1];
+
     for (int i = 0; i < ngpus; i++)
     {
-        CHECK(cudaSetDevice(i));
-        //test<<<grid, block>>>(d_u1[i], isize);
-        void *kernelArgs[] = { 
-            &d_u1[i]
-        };
-        cudaLaunchCooperativeKernel((void*) test, grid, block, kernelArgs);
+        args[i][0] = &d_u1[i];
+    }
+
+    for (int i = 0; i < ngpus; i++)
+    {
+        launchParams[i].func = (void*)test;
+        launchParams[i].gridDim = grid;
+        launchParams[i].blockDim = block;
+        launchParams[i].sharedMem = 0;
+        launchParams[i].stream = streams[i];
+        launchParams[i].args = args[i];
     }
 
     for (int i = 0; i < ngpus; i++)
     {
         CHECK(cudaSetDevice(i));
-        CHECK(cudaMemcpy(&host_ref[isize * i], d_u1[i], ibyte, cudaMemcpyDeviceToHost));
+        //test<<<grid, block>>>(d_u1[i], isize);
+        //cudaLaunchCooperativeKernel(
+        //    launchParams[i].func,
+        //    launchParams[i].gridDim,
+        //    launchParams[i].blockDim,
+        //    launchParams[i].args,
+        //    launchParams[i].sharedMem,
+        //    launchParams[i].stream
+        //);
+        //
+
+        cudaLaunchCooperativeKernel(
+            launchParams[i].func,
+            launchParams[i].gridDim,
+            launchParams[i].blockDim,
+            launchParams[i].args,
+            launchParams[i].sharedMem,
+            launchParams[i].stream
+        );
+    }
+
+    for (int i = 0; i < ngpus; i++)
+    {
+        CHECK(cudaSetDevice(i));
+        CHECK(cudaMemcpyAsync(&host_ref[isize * i], d_u1[i], ibyte, cudaMemcpyDeviceToHost, streams[i]));
+    }
+
+    for (int i = 0; i < ngpus; i++)
+    {
+        CHECK(cudaSetDevice(i));
+        CHECK(cudaStreamDestroy(streams[i]));
     }
 
     for (int d = 0; d < ngpus; d++) {
@@ -59,4 +100,8 @@ int main(int argc, char *argv[]) {
     }
 
     CHECK(cudaDeviceReset());
+
+    free(launchParams);
+    free(streams);
+    free(host_ref);
 }
