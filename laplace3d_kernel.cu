5395d961dd26e6201925344a901fcc06a05e191e
Martin Rebne Farstad <martinrf@stud.ntnu.no>
2020-11-08 16:28:36 +0100

    add base without errors

--- laplace3d_kernel.cu
+++ laplace3d_kernel.cu
@@ -5,11 +5,11 @@
 // device code
 //
 #ifndef BLOCK_X
-#define BLOCK_X 128
+#define BLOCK_X 64
 #endif
 
 #ifndef BLOCK_Y
-#define BLOCK_Y 1
+#define BLOCK_Y 2
 #endif
 
 #ifndef BLOCK_Z
@@ -36,8 +36,7 @@ using namespace cooperative_groups;
 __global__ void GPU_laplace3d(const float* __restrict__ d_u1,
 			      float* __restrict__ d_u2)
 {
-  int   i, j, k, idx, sdx, ioff, joff, koff;
-  const int npad=1, npad2=2;
+  int   i, j, k, tx, ty, tz, idx, ioff, joff, koff;
   float u2, sixth=1.0f/6.0f;
 
   thread_block tb = this_thread_block();
@@ -50,22 +49,19 @@ __global__ void GPU_laplace3d(const float* __restrict__ d_u1,
   j    = threadIdx.y + blockIdx.y*BLOCK_Y;
   k    = threadIdx.z + blockIdx.z*BLOCK_Z;
 
+  tx   = threadIdx.x;
+  ty   = threadIdx.y;
+  tz   = threadIdx.z;
+  
+
   ioff = 1;
   joff = NX;
   koff = NX*NY;
 
   idx = i + j*joff + k*koff;
-  sdx = threadIdx.x + 1;
 
-  __shared__ float xval[BLOCK_X+2];
-  if (threadIdx.x == 0) {
-    if (blockIdx.x != 0) {
-        xval[threadIdx.x] = d_u1[idx-1];
-    } else if (blockIdx.x != gridDim.x-1) {
-        xval[sdx+BLOCK_X] = d_u1[idx+BLOCK_X];
-    }
-  }
-  xval[sdx] = d_u1[idx];
+  __shared__ float smem[BLOCK_Z][BLOCK_Y][BLOCK_X];
+  smem[tz][ty][tx] = d_u1[idx];
   __syncthreads();
 
   if (idx < NX*NY*NZ) {
@@ -75,26 +71,21 @@ __global__ void GPU_laplace3d(const float* __restrict__ d_u1,
       }
       else {
         float tmp = 0.0f;
-        if (threadIdx.x == 0 && blockIdx.x == 0) {
+        if (tx > 0) {
+          tmp += smem[tz][ty][tx-ioff];
+        } else {
           tmp += d_u1[idx-ioff];
-        } else {
-          tmp += xval[sdx-ioff];
         }
-        if (threadIdx.x == BLOCK_X-1 && blockIdx.x == gridDim.x-1) {
+        if (tx < BLOCK_X-1) {
+          tmp += smem[tz][ty][tx+ioff];
+        } else {
           tmp += d_u1[idx+ioff];
-        } else {
-          tmp += xval[sdx+ioff];
         }
-        float jval[] ={
-          d_u1[idx-joff],
-          d_u1[idx+joff]
-        };
-        float kval[] ={
-          d_u1[idx-koff],
-          d_u1[idx+koff]
-        };
-        for (int d=0; d<2; d++) tmp += jval[d] + kval[d];
-        u2 = tmp * sixth;
+
+        u2 = (d_u1[idx-joff]  +
+              d_u1[idx+joff]  +
+              d_u1[idx-koff]  +
+              d_u1[idx+koff]  + tmp) * sixth;
       }
       d_u2[idx] = u2;
   }
