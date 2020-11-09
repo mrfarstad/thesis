f6aab5c04b65bf990dd5fdc0bebaad87c575b70c
Martin Rebne Farstad <martinrf@stud.ntnu.no>
2020-11-09 13:47:41 +0100

    feat: Finish inter-block sync version
    This version contains Cooperative Groups for inter-block synchronization

--- laplace2d.cu
+++ laplace2d.cu
@@ -1,4 +1,12 @@
 #include <stdio.h>
+#include "helper_cuda.h"
+#include "laplace2d_timer.h"
+#include "laplace2d_utils.cu"
+#include "laplace2d_kernel.cu"
+#include "laplace2d_initializer.h"
+#include "laplace2d_error_checker.h"
+//#include "cooperative_groups.h"
+//using namespace cooperative_groups;
 
 #define CU checkCudaErrors 
 #define start_timer cudaEventRecord
@@ -13,15 +21,8 @@
 
 #define NX 256
 #define NY 256
-#define ITERATIONS 8192 
 
-#include "helper_cuda.h"
-#include "laplace2d_timer.h"
-#include "laplace2d_utils.cu"
-#include "laplace2d_kernel.cu"
-#include "laplace2d_initializer.h"
-#include "laplace2d_error_checker.h"
-#include "cooperative_groups.h"
+#define ITERATIONS 8096
 
 void cpu_laplace2d(int nx, int ny, float* h_u1, float* h_u2);
 
@@ -29,7 +30,7 @@ int main(int argc, const char **argv){
     int    i, j, ind,
            ibyte = NX*NY * sizeof(float);
     float  *h_u1, *h_u2, *h_u3, *h_swap,
-           *d_u1, *d_u2, *d_foo,
+           *d_u1, *d_u2, *d_tmp,
            milli;
 
     cudaEvent_t start, stop;
@@ -53,10 +54,17 @@ int main(int argc, const char **argv){
         1 + (NX-1)/BLOCK_X,
         1 + (NY-1)/BLOCK_Y
     );
+
+    int nx = NX;
+    int ny = NY;
+    int iter = ITERATIONS;
     
     void *args[] = {
         &d_u1,
-        &d_u2
+        &d_u2,
+        (void *)&nx,
+        (void *)&ny,
+        (void *)&iter
     };
 
     int device = 0;
@@ -64,44 +72,35 @@ int main(int argc, const char **argv){
     cudaGetDeviceProperties(&deviceProp, device);
     // initialize, then launch
     printf("Multiprocessorcount: %d, Grid: x: %d y: %d\n", deviceProp.multiProcessorCount, dimGrid.x, dimGrid.y);
-    //cudaLaunchCooperativeKernel((void*)gpu_laplace2d, deviceProp.multiProcessorCount, dimBlock, args);
 
-    //start_timer(start);
-    //for (i = 1; i <= ITERATIONS; ++i) {
-    //  gpu_laplace2d<<<dimGrid, dimBlock>>>(
-    //          d_u1,
-    //          d_u2
-    //  );
-    //  getLastCudaError("GPU_laplace3d execution failed\n");
-
-    //  d_foo = d_u1; d_u1 = d_u2; d_u2 = d_foo;   // swap d_u1 and d_u2
-    //}
-    //stop_timer(&start, &stop, &milli, "\nGPU_laplace3d: %.1f (ms) \n");
-    //
-    //start_timer(start);
-    //CU(cudaMemcpy(h_u2, d_u1, ibyte, cudaMemcpyDeviceToHost));
-    //stop_timer(&start, &stop, &milli, "\ncudaMemcpyDeviceToHost: %.1f (ms) \n");
+    start_timer(start);
+    cudaLaunchCooperativeKernel((void*)gpu_laplace2d, deviceProp.multiProcessorCount, dimBlock, args);
+    stop_timer(&start, &stop, &milli, "\ngpu_laplace2d (cooperative groups): %.1f (ms) \n");
+    
+    start_timer(start);
+    CU(cudaMemcpy(h_u2, d_u1, ibyte, cudaMemcpyDeviceToHost));
+    stop_timer(&start, &stop, &milli, "\ncudaMemcpyDeviceToHost: %.1f (ms) \n");
 
 
-    //start_timer(start);
-    //for (i = 1; i <= ITERATIONS; ++i) {
-    //    cpu_laplace2d(NX, NY, h_u1, h_u3);
-    //    h_swap = h_u1; h_u1 = h_u3; h_u3 = h_swap;   // swap h_u1 and h_u3
-    //}
-    //stop_timer(&start, &stop, &milli, "\nCPU_laplace3d: %.1f (ms) \n");
+    start_timer(start);
+    for (i = 1; i <= ITERATIONS; ++i) {
+        cpu_laplace2d(NX, NY, h_u1, h_u3);
+        h_swap = h_u1; h_u1 = h_u3; h_u3 = h_swap;   // swap h_u1 and h_u3
+    }
+    stop_timer(&start, &stop, &milli, "\ncpu_laplace2d: %.1f (ms) \n");
 
-    //check_domain_errors(h_u1, h_u2);
+    check_domain_errors(h_u1, h_u2);
 
-    //// print out corner of array
-    //for (j=0; j<8; j++) {
-    //  for (i=0; i<8; i++) {
-    //    ind = i + j*NX;
-    //    printf(" %5.2f ", h_u2[ind]);
-    //  }
-    //  printf("\n");
-    //}
+    // print out corner of array
+    for (j=0; j<8; j++) {
+      for (i=0; i<8; i++) {
+        ind = i + j*NX;
+        printf(" %5.2f ", h_u2[ind]);
+      }
+      printf("\n");
+    }
 
-    //saveResult(h_u1);
+    saveResult(h_u1);
 
     CU(cudaFree(d_u1));
     CU(cudaFree(d_u2));
--- laplace2d_kernel.cu
+++ laplace2d_kernel.cu
@@ -4,94 +4,64 @@
 
 // device code
 //
-#ifndef BLOCK_X
-#define BLOCK_X 64
-#endif
-
-#ifndef BLOCK_Y
-#define BLOCK_Y 2
-#endif
-
-#ifndef NX
-#define NX 256
-#endif
-
-#ifndef NY
-#define NY 256
-#endif
 
 #include "cooperative_groups.h"
 using namespace cooperative_groups;
 
-__global__ void gpu_laplace2d(const float* __restrict__ d_u1,
-			      float* __restrict__ d_u2)
+__global__ void gpu_laplace2d(float* __restrict__ d_u1,
+			      float* __restrict__ d_u2,
+                              int nx,
+                              int ny,
+                              int iter)
 {
-  int   i, j, k,
-        tx, ty, tz,
-        bx, by, bz,
-        gx, gy, gz,
-        blockx, blocky, blockz,
-        idx, sx, sy, sz, ioff, joff, koff;
-  float u2, fourth=1.0f/4.0f;
+    int   i, j, q,
+          bx, by,
+          gx, gy,
+          xskip, yskip, 
+          idx, ioff, joff;
+    float u2, *d_tmp, fourth=1.0f/4.0f;
+    
+    //
+    // define global indices and array offsets
+    //
+    //
+    bx = blockDim.x;
+    by = blockDim.y;
 
-  thread_block tb = this_thread_block();
-
-  //
-  // define global indices and array offsets
-  //
-  //
-  i    = threadIdx.x + blockIdx.x*BLOCK_X;
-  j    = threadIdx.y + blockIdx.y*BLOCK_Y;
-
-  tx   = threadIdx.x;
-  ty   = threadIdx.y;
-  tz   = threadIdx.z;
-
-  bx   = blockIdx.x;
-  by   = blockIdx.y;
-  bz   = blockIdx.z;
-
-  gx   = gridDim.x;
-  gy   = gridDim.y;
-  gz   = gridDim.z;
-
-  sx = tx+1;
-  sy = ty+1;
-  sz = tz+1;
-
-  blockx = BLOCK_X;
-  blocky = BLOCK_Y;
-
-  i    = threadIdx.x + blockIdx.x*BLOCK_X;
-  j    = threadIdx.y + blockIdx.y*BLOCK_Y;
-
-  ioff = 1;
-  joff = NX;
-
-  idx = i + j*joff;
-
-  __shared__ float smem[BLOCK_Y+2][BLOCK_X+2];
-
-  if (tx == 0 && bx != 0)           smem[sy][tx]   = d_u1[idx-ioff];
-  if (tx == blockx-1 && bx != gx-1) smem[sy][sx+1] = d_u1[idx+ioff];
-
-  if (ty == 0 && by != 0)           smem[ty][sx]   = d_u1[idx-joff];
-  if (ty == blocky-1 && by != gy-1) smem[sy+1][sx] = d_u1[idx+joff];
-  
-  smem[sy][sx] = d_u1[idx];
-
-  tb.sync();
-
-  if (idx < NX*NY) {
-      if (i==0 || i==NX-1 || j==0 || j==NY-1) {
-        u2 = d_u1[idx]; // Dirichlet b.c.'s
-      }
-      else {
-        u2 = (smem[sy][sx-1]  +
-              smem[sy][sx+1]  +
-              smem[sy-1][sx]  +
-              smem[sy+1][sx]) * fourth;    
-      }
-      d_u2[idx] = u2;
-  }
+    i  = threadIdx.x + blockIdx.x*bx;
+    j  = threadIdx.y + blockIdx.y*by;
+    //
+    //
+    gx = gridDim.x;
+    gy = gridDim.y;
+    //
+    xskip = bx * gx;
+    yskip = by * gy;
+    
+    ioff = 1;
+    joff = nx;
+    grid_group g = this_grid();
+    
+    for (q = 1; q <= iter; q++) {
+        for (int y=j; y<ny; y+=yskip) {
+            for (int x=i; x<nx; x+=xskip) {
+                idx = x + y*joff;
+        
+                if (idx < nx*ny) {
+                    if (x==0 || x==nx-1 || y==0 || y==ny-1) {
+                      u2 = d_u1[idx]; // Dirichlet b.c.'s
+                    }
+                    else {
+                      u2 = (d_u1[idx-ioff]  +
+                            d_u1[idx+ioff]  +
+                            d_u1[idx-joff]  +
+                            d_u1[idx+joff]) * fourth;    
+                    }
+                    d_u2[idx] = u2;
+                }
+            }
+        }
+        d_tmp = d_u1; d_u1 = d_u2; d_u2 = d_tmp; // swap d_u1 and d_u2
+        g.sync();
+    }
 }
