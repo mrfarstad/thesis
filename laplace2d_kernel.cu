bd2aa0d1ff71d87deca8a9e02dd52b27761ea585
Martin Rebne Farstad <martinrf@stud.ntnu.no>
2020-11-09 12:25:01 +0100

    feat: Add 2D smem implementation

--- laplace2d_kernel.cu
+++ laplace2d_kernel.cu
@@ -26,8 +26,12 @@ using namespace cooperative_groups;
 __global__ void GPU_laplace3d(const float* __restrict__ d_u1,
 			      float* __restrict__ d_u2)
 {
-  int   i, j,
-        idx, ioff, joff;
+  int   i, j, k,
+        tx, ty, tz,
+        bx, by, bz,
+        gx, gy, gz,
+        blockx, blocky, blockz,
+        idx, sx, sy, sz, ioff, joff, koff;
   float u2, fourth=1.0f/4.0f;
 
   thread_block tb = this_thread_block();
@@ -35,6 +39,28 @@ __global__ void GPU_laplace3d(const float* __restrict__ d_u1,
   //
   // define global indices and array offsets
   //
+  //
+  i    = threadIdx.x + blockIdx.x*BLOCK_X;
+  j    = threadIdx.y + blockIdx.y*BLOCK_Y;
+
+  tx   = threadIdx.x;
+  ty   = threadIdx.y;
+  tz   = threadIdx.z;
+
+  bx   = blockIdx.x;
+  by   = blockIdx.y;
+  bz   = blockIdx.z;
+
+  gx   = gridDim.x;
+  gy   = gridDim.y;
+  gz   = gridDim.z;
+
+  sx = tx+1;
+  sy = ty+1;
+  sz = tz+1;
+
+  blockx = BLOCK_X;
+  blocky = BLOCK_Y;
 
   i    = threadIdx.x + blockIdx.x*BLOCK_X;
   j    = threadIdx.y + blockIdx.y*BLOCK_Y;
@@ -44,16 +70,27 @@ __global__ void GPU_laplace3d(const float* __restrict__ d_u1,
 
   idx = i + j*joff;
 
+  __shared__ float smem[BLOCK_Y+2][BLOCK_X+2];
+
+  if (tx == 0 && bx != 0)           smem[sy][tx]   = d_u1[idx-ioff];
+  if (tx == blockx-1 && bx != gx-1) smem[sy][sx+1] = d_u1[idx+ioff];
+
+  if (ty == 0 && by != 0)           smem[ty][sx]   = d_u1[idx-joff];
+  if (ty == blocky-1 && by != gy-1) smem[sy+1][sx] = d_u1[idx+joff];
+  
+  smem[sy][sx] = d_u1[idx];
+
+  tb.sync();
+
   if (idx < NX*NY) {
       if (i==0 || i==NX-1 || j==0 || j==NY-1) {
-        // Dirichlet b.c.'s
-        u2 = d_u1[idx];
+        u2 = d_u1[idx]; // Dirichlet b.c.'s
       }
       else {
-        u2 = (d_u1[idx - ioff]
-           +  d_u1[idx + ioff]
-           +  d_u1[idx - joff]
-           +  d_u1[idx + joff]) * fourth;
+        u2 = (smem[sy][sx-1]  +
+              smem[sy][sx+1]  +
+              smem[sy-1][sx]  +
+              smem[sy+1][sx]) * fourth;    
       }
       d_u2[idx] = u2;
   }
