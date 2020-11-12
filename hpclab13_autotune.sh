be559dd0b4e49e3bebaff32000d3c8dad83dbb96
Martin Rebne Farstad <martinrf@stud.ntnu.no>
2020-11-12 14:13:31 +0100

    feat: Add error output for autotuning
    This feature makes it easier to test if your solutions gives errors

--- hpclab13_autotune.sh
+++ hpclab13_autotune.sh
@@ -1,1 +1,2 @@
 stdbuf -o 0 -e 0 ./autotune.sh hpclab13 laplace2d | tee results/out.txt
+awk '{if ($2=="error") print}' results/out.txt | tee results/errors.txt
--- yme_autotune.sh
+++ yme_autotune.sh
@@ -2,4 +2,5 @@ scp -r ./* yme:~/thesis_autotune
 ssh yme -t "
     cd thesis_autotune;
     stdbuf -o 0 -e 0 ./autotune.sh yme laplace2d | tee results/out.txt
+    awk '{if ($2=="error") print}' results/out.txt | tee results/errors.txt
     "
