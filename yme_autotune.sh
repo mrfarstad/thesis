df516b8912f89924d0aed62beef3885e431f65b3
Martin Rebne Farstad <martinrf@stud.ntnu.no>
2020-11-12 20:05:19 +0100

    feat: Improve retrieving errors from output

--- yme_autotune.sh
+++ yme_autotune.sh
@@ -1,6 +1,7 @@
 scp -r ./* yme:~/thesis_autotune
 ssh yme -t "
     cd thesis_autotune;
-    stdbuf -o 0 -e 0 ./autotune.sh yme laplace2d | tee results/out.txt
-    awk '{if ($2=="error") print}' results/out.txt | tee results/errors.txt
+    stdbuf -o 0 -e 0 ./autotune.sh yme laplace2d | tee results/out.txt;
+    awk '{if (\$1==\"rms\" && \$2==\"error\") print}' results/out.txt | tee results/errors.txt
     "
+
