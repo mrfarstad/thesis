776605bae2491e40641d634814c823947d8e1d03
Martin Rebne Farstad <martinrf@stud.ntnu.no>
2020-11-12 20:05:19 +0100

    feat: Improve retrieving errors from output

--- hpclab13_autotune.sh
+++ hpclab13_autotune.sh
@@ -1,2 +1,2 @@
 stdbuf -o 0 -e 0 ./autotune.sh hpclab13 laplace2d | tee results/out.txt
-awk '{if ($2=="error") print}' results/out.txt | tee results/errors.txt
+awk '{if ($1=="rms" && $2=="error") print}' results/out.txt | tee results/errors.txt
