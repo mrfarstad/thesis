0153467ec021a8e1c140eb3a21dd334dade97f0b
Martin Rebne Farstad <martinrf@stud.ntnu.no>
2020-11-13 08:49:21 +0100

    feat: Shared Memory Basic v1.0

--- constants.h
+++ constants.h
@@ -1,9 +1,9 @@
+// Optimal blockdims for NX=NY=256 and ITERATIONS=8192 (smem_base)
 #ifndef BLOCK_X
 #define BLOCK_X 32
 #endif
-
 #ifndef BLOCK_Y
-#define BLOCK_Y 32
+#define BLOCK_Y 1
 #endif
 
 #define NX 256
--- laplace2d_kernel.cu
+++ laplace2d_kernel.cu
@@ -10,11 +10,10 @@ using namespace cooperative_groups;
 __global__ void gpu_laplace2d(float* __restrict__ d_u1,
 			      float* __restrict__ d_u2)
 {
-    int   i, j, q, x, y,
+    int   i, j,
           tx, ty,
-          xskip, yskip, 
           idx, ioff, joff;
-    float u2, *d_tmp, fourth=1.0f/4.0f;
+    float u2 = 0.0f, fourth=1.0f/4.0f;
     
     //
     // define global indices and array offsets
@@ -25,9 +24,6 @@ __global__ void gpu_laplace2d(float* __restrict__ d_u1,
     i  = tx + blockIdx.x*BLOCK_X;
     j  = ty + blockIdx.y*BLOCK_Y;
 
-    xskip = BLOCK_X * gridDim.x;
-    yskip = BLOCK_Y * gridDim.y;
-
     ioff = 1;
     joff = NX;
 
@@ -35,31 +31,24 @@ __global__ void gpu_laplace2d(float* __restrict__ d_u1,
     thread_block tb = this_thread_block();
     __shared__ float smem[BLOCK_Y][BLOCK_X];
 
-    for (q = 1; q <= ITERATIONS; q++) {
-        for (y=j; y<NY; y+=yskip) {
-            for (x=i; x<NX; x+=xskip) {
-                idx = x + y*joff;
-                smem[ty][tx] = d_u1[idx];
-                tb.sync();
-                if (x==0 || x==NX-1 || y==0 || y==NY-1)
-                  u2 = d_u1[idx]; // Dirichlet b.c.'s
-                else {
-                  float tmp = 0.0f;
-                  if (tx > 0)         tmp += smem[ty][tx-1];
-                  else                tmp += d_u1[idx-ioff];
-                  if (tx < BLOCK_X-1) tmp += smem[ty][tx+1];
-                  else                tmp += d_u1[idx+ioff];
-                  if (ty > 0)         tmp += smem[ty-1][tx];
-                  else                tmp += d_u1[idx-joff];
-                  if (ty < BLOCK_Y-1) tmp += smem[ty+1][tx];
-                  else                tmp += d_u1[idx+joff];
-                  u2 = tmp * fourth;
-                }
-                d_u2[idx] = u2;
-                tb.sync();
-             }
+    idx = i + j*joff;
+    smem[ty][tx] = d_u1[idx];
+    tb.sync();
+    if (i>=0 && i<=NX-1 && j>=0 && j<=NY-1) {
+        if (i==0 || i==NX-1 || j==0 || j==NY-1)
+          u2 = d_u1[idx]; // Dirichlet b.c.'s
+        else {
+              float tmp = 0.0f;
+              if (tx > 0)         tmp += smem[ty][tx-1];
+              else                tmp += d_u1[idx-ioff];
+              if (tx < BLOCK_X-1) tmp += smem[ty][tx+1];
+              else                tmp += d_u1[idx+ioff];
+              if (ty > 0)         tmp += smem[ty-1][tx];
+              else                tmp += d_u1[idx-joff];
+              if (ty < BLOCK_Y-1) tmp += smem[ty+1][tx];
+              else                tmp += d_u1[idx+joff];
+              u2 = tmp * fourth;
         }
-        d_tmp = d_u1; d_u1 = d_u2; d_u2 = d_tmp; // swap d_u1 and d_u2
-        g.sync();
+        d_u2[idx] = u2;
     }
 }
