ea7dacbbbd646019dac588624089af714032074c
Martin Rebne Farstad <martinrf@stud.ntnu.no>
2020-11-13 09:13:26 +0100

    feat: Add basic version v1.0

--- constants.h
+++ constants.h
@@ -1,9 +1,9 @@
+// Optimal blockdims for SX=SY=256 and ITERATIONS=8192 (basic) @ yme
 #ifndef BLOCK_X
-#define BLOCK_X 32
+#define BLOCK_X 128
 #endif
-
 #ifndef BLOCK_Y
-#define BLOCK_Y 32
+#define BLOCK_Y 8
 #endif
 
 #define NX 256
