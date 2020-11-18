020c384f5fd030f5b547c0d6bb7c33079c1dd56b
Martin Rebne Farstad <martinrf@stud.ntnu.no>
2020-11-18 13:23:16 +0100

    fix: Fix blockdim optimization script
    It seemed like it was a race condition causing the tst.txt file to
    contain weird outputs. This should now be fixed. Only tested with a
    subset of the versions and dimensions.

--- find_optimal_block_size.sh
+++ find_optimal_block_size.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
+if [[ $# -eq 0 ]] ; then
+    echo 'arg: hpclab13/yme'
+    exit 0
+fi
 
-#sizes=(256 512 1024 2048 4096 8192 16384 32768)
 iter=1024
-
-#versions=(base smem)
-#coop coop_smem)
-#sizes=(32 64)
-#128 256 512 1024 2048 4096 8192)
-
-versions=(base smem coop coop_smem)
-sizes=(32 64 128 256 512 1024 2048 4096 8192)
+#versions=(base smem coop coop_smem)
+versions=(coop coop_smem)
+#sizes=(32 64 128 256 512 1024 2048 4096 8192)
+sizes=(32 64 128)
 host=$1
 
 #./create_solutions.sh ${sizes[@]}
@@ -27,16 +26,11 @@ do
     do
       :
       echo "$v (NX=NY=$s)" >> tst.txt
-      #perl -i -pe"s/DIM=\d*/DIM=$s/g" configs/hpclab13/$i.conf
-      #sed -i -re 's/(DIM=)[0-9]+/\1'$s'/' configs/yme/$v.conf
       sed -i -re 's/(DIM=)[0-9]+/\1'$s'/' configs/$1/$v.conf
-      stdbuf -o 0 -e 0 ./autotune.sh hpclab13 $v laplace2d > results/out.txt
-      #stdbuf -o 0 -e 0 ./autotune.sh hpclab13 $v laplace2d | tee results/out.txt
-      #./$1_autotune.sh $v
-      #./yme_autotune.sh $v
-      awk '/Minimal valuation/{x=NR+3}(NR<=x){print}' results/out.txt >> tst.txt
+      ###stdbuf -o 0 -e 0 ./autotune.sh hpclab13 $v laplace2d > results/out.txt
+      wait
+      stdbuf -o 0 -e 0 ./autotune.sh hpclab13 $v laplace2d | tee results/out_"$v"_"$s".txt
+      wait
+      awk '/Minimal valuation/{x=NR+3}(NR<=x){print}' results/out_"$v"_"$s".txt >> tst.txt
     done
 done
-
-#stdbuf -o 0 -e 0 ./autotune.sh hpclab13 $1 laplace2d | tee results/out.txt
-#awk '{if ($1=="rms" && $2=="error") print}' results/out.txt | tee results/errors.txt
