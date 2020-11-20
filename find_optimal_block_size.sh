261c1dd760d9315d81de45b43f79cea6bf90caa9
Martin Rebne Farstad <martinrf@stud.ntnu.no>
2020-11-20 10:20:14 +0100

    feat: Result generator for 1 GPU
    This version of the repo was used to generate all results for 1 GPU
    (base, smem, coop, coop_smem). The results reported 0 errors all over.

--- find_optimal_block_size.sh
+++ find_optimal_block_size.sh
@@ -5,9 +5,9 @@ if [[ $# -eq 0 ]] ; then
 fi
 
 iter=1024
-#versions=(base smem coop coop_smem)
 #versions=(coop coop_smem)
-versions=(base smem)
+#versions=(base smem)
+versions=(base smem coop coop_smem)
 sizes=(32 64 128 256 512 1024 2048 4096)
 # 4096 8192)
 #sizes=(32 64 128)
@@ -27,14 +27,14 @@ do
     for s in "${sizes[@]}"
     do
       :
-      echo "$v (NX=NY=$s 4 GPUs)" >> results/duo_gpu.txt
-      sed -i -re 's/(DIM=)[0-9]+/\1'$s'/' configs/$1/$v.conf
+      echo "$v (NX=NY=$s 1 GPU)" >> results/1_gpu.txt
+      sed -i -re 's/(DIM=)[0-9]+/\1'$s'/' configs/$host/$v.conf
       ###stdbuf -o 0 -e 0 ./autotune.sh $host $v laplace2d > results/out.txt
       wait
-      stdbuf -o 0 -e 0 ./autotune.sh $host $v laplace2d | tee results/out_"$v"_"$s"_duo.txt
+      stdbuf -o 0 -e 0 ./autotune.sh $host $v laplace2d | tee results/out_"$v"_"$s".txt
       wait
       # Only use this if DEBUG=true
-      awk '{if ($1=="rms" && $2=="error") print}' results/out_"$v"_"$s"_duo.txt >> results/duo_gpu_errors.txt
-      awk '/Minimal valuation/{x=NR+3}(NR<=x){print}' results/out_"$v"_"$s"_duo.txt >> results/duo_gpu.txt
+      awk '{if ($1=="rms" && $2=="error") print}' results/out_"$v"_"$s".txt >> results/1_gpu_errors.txt
+      awk '/Minimal valuation/{x=NR+3}(NR<=x){print}' results/out_"$v"_"$s".txt >> results/1_gpu.txt
     done
 done
