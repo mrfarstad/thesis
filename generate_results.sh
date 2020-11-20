d3918146fcacd785779ffc4b76cea568e327ec8e
Martin Rebne Farstad <martinrf@stud.ntnu.no>
2020-11-20 12:00:30 +0100

    feat: Generate results for 1 GPU
    TODO: The only config file available is for smem 1 GPU. Fix for multiple
    version and multiple GPUs.

--- generate_results.sh
+++ generate_results.sh
@@ -4,8 +4,8 @@
 #This script should take the domain dim and the block dims as parameters.
 #This is because it is the python script who has generated the optimal parameters, and therefore the script will provide them.
 
-if [[ $# -lt 3 ]] ; then
-    echo 'arg: DIM BLOCK_X BLOCK_Y'
+if [[ $# -lt 4 ]] ; then
+    echo 'arg: NGPUS DIM BLOCK_X BLOCK_Y'
     exit 0
 fi
 
@@ -13,7 +13,12 @@ config=smem.conf
 iter=1024
 [ ! -f solutions/solution\_$1\_$iter ] && ./create_solutions.sh $1
 wait
-sed -i -re 's/(DIM = )[0-9]+/\1'$1'/' $config
-sed -i -re 's/(BLOCK_X = )[0-9]+/\1'$2'/' $config
-sed -i -re 's/(BLOCK_Y = )[0-9]+/\1'$3'/' $config
-python ${PWD}/Autotuning/tuner/tune.py $config
+sed -i -re 's/(NGPUS = )[0-9]+/\1'$1'/' $config
+sed -i -re 's/(DIM = )[0-9]+/\1'$2'/' $config
+sed -i -re 's/(BLOCK_X = )[0-9]+/\1'$3'/' $config
+sed -i -re 's/(BLOCK_Y = )[0-9]+/\1'$4'/' $config
+stdbuf -o 0 -e 0 python ${PWD}/Autotuning/tuner/tune.py $config | tee /dev/tty | awk '/rms error/{x=NR+1}(NR<=x){print}' | awk '$0==($0+0)'
+#awk '/rms error/{x=NR+1}(NR<=x){print}' gen.txt | awk '$0==($0+0)'
+
+
+# Forstår ikke helt resultatene mine.. Alle er jo smem wtf. Hvorfor er de forskjellige?
--- handle_results.py
+++ handle_results.py
@@ -1,26 +1,32 @@
+import json as j
 import math
 import pprint as p
+import subprocess
 
 domain_dims = [32, 64, 128, 256, 512, 1024, 2048, 4096]
 # NOTE: The 2_gpus output is a copy of 4_gpus. The real output is being generated.
-folders = ["2_gpus/2_gpus.txt", "4_gpus/4_gpus.txt"]
+prefix = "results/yme/"
+folders = ["1_gpu/1_gpu.txt", "2_gpus/2_gpus.txt", "4_gpus/4_gpus.txt"]
 
 db = {}
 
 for folder in folders:
     gpus = folder.split('/')[0]
     db[gpus] = {}
-    with open(folder, "r") as file:
+    ngpus = folder.split('_')[0]
+    with open(prefix + folder, "r") as file:
         lines = file.readlines()
         for i in range(0, len(lines), 5):
             tmp = lines[i].split(' ')
             version = tmp[0]
             db[gpus][version] = {}
 
+        #for i in range(0, 6, 5):
         for i in range(0, len(lines), 5):
             tmp = lines[i].split(' ')
             version = tmp[0]
 
+            #domain_dim = int(tmp[1].split("=")[-1].strip())
             domain_dim = tmp[1].split("=")[-1].strip()
             tmp_db = {}
             params = lines[i+1:i+5]
@@ -31,11 +37,24 @@ for folder in folders:
             blockdims = {}
             for cfg in ttmp:
                 block = cfg[0].strip()
-                blockdim = int(cfg[1].strip())
+                blockdim = cfg[1].strip()
                 blockdims[block] = blockdim
 
+            res = subprocess.run(
+                    ['./generate_results.sh',
+                     ngpus,
+                     domain_dim,
+                     blockdims['BLOCK_X'],
+                     blockdims['BLOCK_Y']],
+                    stdout=subprocess.PIPE).stdout.decode('utf-8')
+            results = list(filter(None, res.split('\n')))
+            blockdims["results"] = [float(result) for result in results]#[res.strip() for res in results]
             db[gpus][version][domain_dim] = blockdims
 
+pretty_db = p.pformat(db)
+print(pretty_db)
+with open('results.txt', 'w') as out_file:
+     out_file.write(pretty_db)
 
-
-p.pprint(db)
+with open('results.json', 'w') as fp:
+    j.dump(db, fp)
