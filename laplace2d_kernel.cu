b027dfba7fe2691666fb9de31b3457eab1d3a8f3
Martin Rebne Farstad <martinrf@stud.ntnu.no>
2020-12-02 14:52:18 +0100

    chore: Fix indending and remove redundant code

--- laplace2d_kernel.cu
+++ laplace2d_kernel.cu
@@ -16,8 +16,8 @@ __global__ void gpu_laplace2d_base(float* __restrict__ d_u1,
         if (i==0 || i==NX-1 || j==jstart || j==jend)
           u2 = d_u1[idx]; // Dirichlet boundary conditions
         else {
-          u2 = (d_u1[idx-1]  +
-                d_u1[idx+1]  +
+          u2 = (d_u1[idx-1]   +
+                d_u1[idx+1]   +
                 d_u1[idx-NX]  +
                 d_u1[idx+NX]) * fourth;
         }
@@ -36,15 +36,14 @@ __global__ void gpu_laplace2d_smem(float* __restrict__ d_u1,
     sy = threadIdx.y+1;
     i  = threadIdx.x + blockIdx.x*BLOCK_X;
     j  = threadIdx.y + blockIdx.y*BLOCK_Y;
-    grid_group g = this_grid();
     thread_block tb = this_thread_block();
     __shared__ float smem[BLOCK_Y+2][BLOCK_X+2];
     idx = i + j*NX;
     if (i>=0 && i<=NX-1 && j>=jstart && j<=jend) {
-        if (i != 0)           smem[sy][sx-1]   = d_u1[idx-1];
-        if (i != NX-1)        smem[sy][sx+1]   = d_u1[idx+1];
-        if (j != jstart)      smem[sy-1][sx]   = d_u1[idx-NX];
-        if (j != jend)        smem[sy+1][sx]   = d_u1[idx+NX];
+        if (i != 0)      smem[sy][sx-1]   = d_u1[idx-1];
+        if (i != NX-1)   smem[sy][sx+1]   = d_u1[idx+1];
+        if (j != jstart) smem[sy-1][sx]   = d_u1[idx-NX];
+        if (j != jend)   smem[sy+1][sx]   = d_u1[idx+NX];
         smem[sy][sx] = d_u1[idx];
     }
     tb.sync();
@@ -80,8 +79,8 @@ __global__ void gpu_laplace2d_coop(float* __restrict__ d_u1,
                 if (x==0 || x==NX-1 || y==0 || y==NY-1)
                   u2 = d_u1[idx]; // Dirichlet boundary conditions
                 else {
-                  u2 = (d_u1[idx-1]  +
-                        d_u1[idx+1]  +
+                  u2 = (d_u1[idx-1]   +
+                        d_u1[idx+1]   +
                         d_u1[idx-NX]  +
                         d_u1[idx+NX]) * fourth;    
                 }
@@ -112,10 +111,10 @@ __global__ void gpu_laplace2d_coop_smem(float* __restrict__ d_u1,
             for (x=i; x<NX; x+=xskip) {
                 idx = x + y*NX;
                 tb.sync();
-                if (x != 0)           smem[sy][sx-1]   = d_u1[idx-1];
-                if (x != NX-1)        smem[sy][sx+1]   = d_u1[idx+1];
-                if (y != 0)           smem[sy-1][sx]   = d_u1[idx-NX];
-                if (y != NY-1)        smem[sy+1][sx]   = d_u1[idx+NX];
+                if (x != 0)    smem[sy][sx-1]   = d_u1[idx-1];
+                if (x != NX-1) smem[sy][sx+1]   = d_u1[idx+1];
+                if (y != 0)    smem[sy-1][sx]   = d_u1[idx-NX];
+                if (y != NY-1) smem[sy+1][sx]   = d_u1[idx+NX];
                 smem[sy][sx] = d_u1[idx];
                 tb.sync();
                 if (x==0 || x==NX-1 || y==0 || y==NY-1) {
