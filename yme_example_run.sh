d539726151118cc7d3af88fd3da559496d086ad7
Martin Rebne Farstad <martinrf@stud.ntnu.no>
2020-12-03 18:42:52 +0100

    fix: Prevent all folders to be checked when using rsync

--- yme_example_run.sh
+++ yme_example_run.sh
@@ -1,7 +1,6 @@
-rsync -v -r ./* yme:~/thesis_autotune
+#!/bin/bash
+rsync --exclude={'solutions/','results/','result_*'} -v -r ./* yme:~/thesis_autotune
 ssh yme -t "
     cd thesis_autotune;
-    stdbuf -o 0 -e 0 ./run.sh $1 prod 32 32 yme | tee results/out.txt;
+    stdbuf -o 0 -e 0 ./run.sh base prod 32 32 2048 yme | tee results/out.txt;
     "
-
- 
