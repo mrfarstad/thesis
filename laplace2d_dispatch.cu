1463d3985a0715829a4dd2f70fabd38999487909
Martin Rebne Farstad <martinrf@stud.ntnu.no>
2020-12-04 14:54:52 +0100

    chore: Format code to fit into thesis

--- laplace2d_dispatch.cu
+++ laplace2d_dispatch.cu
@@ -3,12 +3,12 @@
 #include "laplace2d_kernel.cu"
 
 void dispatch_kernels(float *d_u1, float *d_u2) {
-    dim3 dimBlock(BLOCK_X,BLOCK_Y);
-    dim3 dimGrid(1 + (NX-1)/BLOCK_X, 1 + (NY-1)/BLOCK_Y);
+    dim3 block(BLOCK_X,BLOCK_Y);
+    dim3 grid(1 + (NX-1)/BLOCK_X, 1 + (NY-1)/BLOCK_Y);
     float *d_tmp;
     for (int i=0; i<ITERATIONS; i++) {
-        if (SMEM) gpu_laplace2d_smem<<<dimGrid, dimBlock>>>(d_u1, d_u2, 0, NY-1);
-        else      gpu_laplace2d_base<<<dimGrid, dimBlock>>>(d_u1, d_u2, 0, NY-1);
+        if (SMEM) gpu_laplace2d_smem<<<grid, block>>>(d_u1, d_u2, 0, NY-1);
+        else      gpu_laplace2d_base<<<grid, block>>>(d_u1, d_u2, 0, NY-1);
         getLastCudaError("gpu_laplace2d execution failed\n");
         d_tmp = d_u1; d_u1 = d_u2; d_u2 = d_tmp; // swap d_u1 and d_u2
     }
@@ -18,20 +18,28 @@ void dispatch_cooperative_groups_kernels(float *d_u1, float *d_u2) {
     int device = 0;
     cudaDeviceProp deviceProp;
     cudaGetDeviceProperties(&deviceProp, device);
-    dim3 dimBlock(BLOCK_X,BLOCK_Y);
-    dim3 dimGrid(deviceProp.multiProcessorCount, 1);
+    dim3 block(BLOCK_X,BLOCK_Y);
+    dim3 grid(deviceProp.multiProcessorCount, 1);
     void *args[] = {
         &d_u1,
         &d_u2
     };
-    if (SMEM) cudaLaunchCooperativeKernel((void*)gpu_laplace2d_coop_smem, dimGrid, dimBlock, args);
-    else cudaLaunchCooperativeKernel((void*)gpu_laplace2d_coop, dimGrid, dimBlock, args);
+    if (SMEM)
+        cudaLaunchCooperativeKernel((void*)gpu_laplace2d_coop_smem,
+                                    grid,
+                                    block,
+                                    args);
+    else
+        cudaLaunchCooperativeKernel((void*)gpu_laplace2d_coop,
+                                    grid,
+                                    block,
+                                    args);
     getLastCudaError("gpu_laplace2d execution failed\n");
 }
 
 void dispatch_multi_gpu_kernels(float **d_u1, float **d_u2) {
-    dim3 dimBlock(BLOCK_X,BLOCK_Y);
-    dim3 dimGrid(1 + (NX-1)/BLOCK_X, 1 + (NY-1)/BLOCK_Y);
+    dim3 block(BLOCK_X,BLOCK_Y);
+    dim3 grid(1 + (NX-1)/BLOCK_X, 1 + (NY-1)/BLOCK_Y);
     float *d_tmp;
     int i, s, n;
     int jstart, jend;
@@ -43,16 +51,24 @@ void dispatch_multi_gpu_kernels(float **d_u1, float **d_u2) {
         for (s=0; s<NGPUS; s++) {
             cudaSetDevice(s);
             if (s==0)
-                CU(cudaMemcpy(d_u1[s] + (top+1) * NX, d_u1[s+1] + bot * NX,
-                                   BORDER_BYTES, cudaMemcpyDeviceToDevice));
+                CU(cudaMemcpy(d_u1[s] + (top+1) * NX,
+                              d_u1[s+1] + bot * NX,
+                              BORDER_BYTES,
+                              cudaMemcpyDeviceToDevice));
             else if (s==NGPUS-1)
-                CU(cudaMemcpy(d_u1[s] + 0, d_u1[s-1] + top * NX,
-                                   BORDER_BYTES, cudaMemcpyDeviceToDevice));
+                CU(cudaMemcpy(d_u1[s],
+                              d_u1[s-1] + top * NX,
+                              BORDER_BYTES,
+                              cudaMemcpyDeviceToDevice));
             else {
-                CU(cudaMemcpy(d_u1[s] + 0, d_u1[s-1] + top * NX,
-                                   BORDER_BYTES, cudaMemcpyDeviceToDevice));
-                CU(cudaMemcpy(d_u1[s] + (top+1) * NX, d_u1[s+1] + bot * NX,
-                                   BORDER_BYTES, cudaMemcpyDeviceToDevice));
+                CU(cudaMemcpy(d_u1[s],
+                              d_u1[s-1] + top * NX,
+                              BORDER_BYTES,
+                              cudaMemcpyDeviceToDevice));
+                CU(cudaMemcpy(d_u1[s] + (top+1) * NX,
+                              d_u1[s+1] + bot * NX,
+                              BORDER_BYTES,
+                              cudaMemcpyDeviceToDevice));
             }
         }
         for (n = 0; n < HALO_DEPTH; n++) {
@@ -70,8 +86,16 @@ void dispatch_multi_gpu_kernels(float **d_u1, float **d_u2) {
                     jstart = 0;
                     jend = top+HALO_DEPTH;
                 }
-                if (SMEM) gpu_laplace2d_smem<<<dimGrid, dimBlock>>>(d_u1[s], d_u2[s], jstart, jend);
-                else      gpu_laplace2d_base<<<dimGrid, dimBlock>>>(d_u1[s], d_u2[s], jstart, jend);
+                if (SMEM)
+                    gpu_laplace2d_smem<<<grid, block>>>(d_u1[s],
+                                                        d_u2[s],
+                                                        jstart,
+                                                        jend);
+                else
+                    gpu_laplace2d_base<<<grid, block>>>(d_u1[s],
+                                                        d_u2[s],
+                                                        jstart,
+                                                        jend);
                 getLastCudaError("gpu_laplace2d execution failed\n");
             }
             for (s=0; s<NGPUS; s++) {
