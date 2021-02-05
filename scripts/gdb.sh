d921593284346129c691443e0d2fd0458c866db2
Martin Rebne Farstad <martinrf@stud.ntnu.no>
2021-02-05 16:13:49 +0100

    feat: Add cuda-gdb script for yme

--- scripts/gdb.sh
+++ scripts/gdb.sh
@@ -3,6 +3,6 @@ project_folder=$(echo ${PWD} | sed 's/thesis.*/thesis/')
 source $project_folder/constants.sh
 #make -C $project_folder laplace3d_cpu
 #$project_folder/bin/laplace3d_cpu
-$project_folder/scripts/build.sh debug hpclab13
+$project_folder/scripts/build.sh debug yme
 #make -C $project_folder ID=debug BUILD=debug BLOCK_X=32 BLOCK_Y=32 HOST=hpclab13 && 
 cuda-gdb $project_folder/bin/laplace3d_debug
---
+++ scripts/yme/yme_gdb.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+project_folder=$(echo ${PWD} | sed 's/thesis.*/thesis/')
+source $project_folder/constants.sh
+rsync --exclude={'solutions/','results/'} -v -r ./* yme:~/$YME_WORKING_FOLDER
+ssh yme -t "
+    cd $YME_WORKING_FOLDER;
+    source ./constants.sh;
+    ./scripts/build.sh debug yme
+    cuda-gdb ./bin/laplace3d_debug
+    "
