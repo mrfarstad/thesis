eaf5692cebc7ec6aa90ec290b94ca0e8930f79ac
Martin Rebne Farstad <martinrf@stud.ntnu.no>
2021-02-09 15:12:36 +0100

    fix: Fix yme profiling script

--- scripts/yme/yme_profile.sh
+++ scripts/yme/yme_profile.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 project_folder=$(echo ${PWD} | sed 's/thesis.*/thesis/')
 source $project_folder/constants.sh
-build=prod
+build=profile
+output=bin/profile.prof
 rsync --exclude={'solutions/','results/'} -v -r ./* yme:~/$YME_WORKING_FOLDER
 ssh yme -t "
     cd $YME_WORKING_FOLDER;
     source ./constants.sh
-    ./build.sh $build yme;
-    sudo $(which nvprof) ./bin/laplace2d_$build
+    ./scripts/build.sh $build yme;
+    sudo $(which nvprof) -o $output -f ./bin/laplace3d_$build
     "
-rsync -v -r yme:~/$YME_WORKING_FOLDER/profile .
+rsync -v -r yme:~/$YME_WORKING_FOLDER/$output bin/
 #    sudo $(which nvprof) -o profile -f ./bin/laplace2d_debug
 #
