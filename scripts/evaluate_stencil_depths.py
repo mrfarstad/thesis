0d50e42d5e158a2d25329b4c49467c69efba6248
Martin Rebne Farstad <martinrf@stud.ntnu.no>
2021-03-09 12:37:45 +0100

    feat: Add multiple versions in same plot

--- scripts/evaluate_stencil_depths.py
+++ scripts/evaluate_stencil_depths.py
@@ -7,26 +7,28 @@ gpus = [1, 2, 4]
 stencil_depths = [1, 2, 4, 8, 16, 32]
 
 db = {}
-
 for gpu in gpus:
     db[gpu] = {}
-
 for gpu in gpus:
-    for depth in stencil_depths:
-        res = subprocess.run(
-                ['./scripts/evaluate_stencil_depths.sh',
-                 'base',
-                 str(gpu),
-                 '256',
-                 '32',
-                 '8',
-                 '4',
-                 str(depth)],
-                stdout=subprocess.PIPE).stdout.decode('utf-8')
-        results = list(filter(None, res.split('\n')))
-        db[gpu][depth] = [float(result) for result in results]
-
+    if gpu == 1:
+        versions = ['base', 'smem', 'coop']#, 'coop_smem']
+    else:
+        versions = ['base', 'smem']
+    for version in versions:
+        db[gpu][version] = {}
+        for depth in stencil_depths:
+            res = subprocess.run(
+                    ['./scripts/evaluate_stencil_depths.sh',
+                     version,
+                     str(gpu),
+                     '256',
+                     '32',
+                     '8',
+                     '4',
+                     str(depth)],
+                    stdout=subprocess.PIPE).stdout.decode('utf-8')
+            results = list(filter(None, res.split('\n')))
+            db[gpu][version][depth] = [float(result) for result in results]
 p.pprint(db)
-
 with open('results_stencil_depths.json', 'w') as fp:
     json.dump(db, fp)
