8a2ec72a4ce275970b0e7e7a6f17c22c6cac2d05
Martin Rebne Farstad <martinrf@stud.ntnu.no>
2021-04-21 15:52:28 +0200

    fix: Edit migration script to handle multiple iterations and hosts

--- scripts/migrate_stencil_depths_json.py
+++ scripts/migrate_stencil_depths_json.py
@@ -1,5 +1,6 @@
 import json
 import pprint as p
+import sys
 from functools import reduce
 
 new_db = {}
@@ -7,34 +8,42 @@ new_db = {}
 def deep_get(dictionary, keys, default=None):
     return reduce(lambda d, key: d.get(key, default) if isinstance(d, dict) else default, keys.split("."), dictionary)
 
-def entry_not_exists(nested_list):
-    return deep_get(new_db, ".".join(nested_list)) == None
+def entry_not_exists(db, nested_list):
+    return deep_get(db, ".".join(nested_list)) == None
 
-def copy(config, results_json):
+def copy(config, results_json, host):
     with open(results_json) as file: 
         db = json.loads(file.read())
         for dimension, dimension_db in db.items():
-            if entry_not_exists([dimension]):
+            if entry_not_exists(new_db, [dimension]):
                 new_db[dimension] = {}
             for domain_dim, domain_dim_db in dimension_db.items():
-                if entry_not_exists([dimension, domain_dim]):
+                if entry_not_exists(new_db, [dimension, domain_dim]):
                     new_db[dimension][domain_dim] = {}
                 for version, version_db in domain_dim_db.items():
-                    if entry_not_exists([dimension, domain_dim, version]):
+                    version = version.replace("smem_prefetch", "smem_padded")
+                    if entry_not_exists(new_db, [dimension, domain_dim, version]):
                         new_db[dimension][domain_dim][version] = {}
-                    for stencil_depth, times_db in version_db.items():
-                        if entry_not_exists([dimension, domain_dim, version, stencil_depth]):
+                    for stencil_depth, stencil_depth_db in version_db.items():
+                        if entry_not_exists(new_db, [dimension, domain_dim, version, stencil_depth]):
                             new_db[dimension][domain_dim][version][stencil_depth] = {}
-                        if entry_not_exists([dimension, domain_dim, version, stencil_depth, "8"]):
-                            new_db[dimension][domain_dim][version][stencil_depth]["8"] = {}
-                        for execution_config, times in times_db.items():
-                            if execution_config == config:
-                                new_db[dimension][domain_dim][version][stencil_depth]["8"][config] = times
+                        for iteration, iteration_db in stencil_depth_db.items():
+                            if entry_not_exists(new_db, [dimension, domain_dim, version, stencil_depth, iteration]):
+                                new_db[dimension][domain_dim][version][stencil_depth][iteration] = {}
+                            if entry_not_exists(new_db, [dimension, domain_dim, version, stencil_depth, iteration, host]):
+                                new_db[dimension][domain_dim][version][stencil_depth][iteration][host] = {}
+                            if entry_not_exists(db, [dimension, domain_dim, version, stencil_depth, iteration, host, config]):
+                                continue
+                            new_db[dimension][domain_dim][version][stencil_depth][iteration][host][config] = iteration_db[host][config]
 
-copy("heuristic", "results/results_stencil_depths_heuristic.json")
-copy("autotune", "results/results_stencil_depths_autotuned.json")
 
-with open('results/results_stencil_depths.json', 'w') as fp:
+copy("heuristic", "results/results_stencil_depths_heuristic.json", "heid")
+copy("autotune", "results/results_stencil_depths_autotuned.json", "heid")
+copy("heuristic", "results/results_stencil_depths_idun.json", "idun")
+
+#with open("results/results_stencil_depths_autotuned.json", 'w') as fp:
+#with open("results/results_stencil_depths_idun.json", 'w') as fp:
+with open("results/results_stencil_depths.json", 'w') as fp:
     json.dump(new_db, fp)
 
 p.pprint(new_db)
