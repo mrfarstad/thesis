4ec9c481852631d7c749fedea8142eba0269cf9a
Martin Rebne Farstad <martinrf@stud.ntnu.no>
2021-05-04 12:59:54 +0200

    feat: Improve metric-comparing script output

--- scripts/compare_metrics.py
+++ scripts/compare_metrics.py
@@ -3,6 +3,7 @@ import pprint as p
 import sys
 import pandas as pd
 from functools import reduce
+import statistics
 
 
 def deep_get(dictionary, keys, default=None):
@@ -19,17 +20,25 @@ def entry_not_exists(db, nested_list):
 
 with open("results/results_stencil_depths.json") as file:
     db = json.loads(file.read())
-versions = [("1_gpus_base", "1"), ("1_gpus_base_unroll_2", "1")]
-metrics = [db["2"]["32768"][v][d]["8"]["heid"]["heuristic"] for v, d in versions]
-for metric_db, version in zip(metrics, [v[0] for v in versions]):
-    metric_db["version"] = version
-    del metric_db["time"]
+versions = [
+    ("1_gpus_smem_padded", "1", "heuristic"),
+    ("1_gpus_smem_padded", "1", "autotune"),
+]
+
+metrics = [db["2"]["32768"][v][d]["8"]["heid"][c] for v, d, c in versions]
+
+for metric_db, version in zip(metrics, versions):  # [v[0] for v in versions]):
+    metric_db["version"] = version[0] + " (%s)" % version[2]
+    metric_db["time"] = statistics.median(metric_db["time"])
     for k, v in metric_db.items():
         metric_db[k] = [v]
+
 dfs = list(map(pd.DataFrame, metrics))
 pd.set_option("display.max_rows", None)
-dfs = pd.concat(dfs)
-dfs.set_index("version", inplace=True)
-dfs = dfs.transpose()
+df = pd.concat(dfs)
+df.set_index("version", inplace=True)
+df = df.reindex(sorted(df.columns), axis=1)
+df = df.transpose()
+# pd.options.display.width = 0
 
-print(dfs)
+print(df)
