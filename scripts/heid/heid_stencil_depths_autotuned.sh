c3c1eb33d075521fe6ec74430c60b2f03822d472
Martin Rebne Farstad <martinrf@stud.ntnu.no>
2021-05-08 10:07:37 +0200

    chore: Update GPU-indexes

--- scripts/heid/heid_stencil_depths_autotuned.sh
+++ scripts/heid/heid_stencil_depths_autotuned.sh
@@ -3,7 +3,7 @@ project_folder=$(echo ${PWD} | sed 's/thesis.*/thesis/')
 
 #gpu_index=all
 #gpu_index=0,1,2,3
-gpu_index=1
+gpu_index=12
 bash $project_folder/scripts/set_cuda_visible_devices.sh $gpu_index
 bash $project_folder/scripts/configure_dockerfile.sh autotune
 bash $project_folder/scripts/heid/heid_docker.sh stencil_depths_autotuned
