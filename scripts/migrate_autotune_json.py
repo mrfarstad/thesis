69196a9d8ce6b6127dedd75dcc7ce480a136a6a1
Martin Rebne Farstad <martinrf@stud.ntnu.no>
2021-05-13 07:45:12 +0200

    feat: Add a script that combines results_autotune.json files
    This script combines the results after parallelizing the
    autotuning by running different versions on different GPUs.

---
+++ scripts/migrate_autotune_json.py
@@ -0,0 +1,49 @@
+import json
+import pprint as p
+import sys
+from functools import reduce
+
+new_db = {}
+
+
+def deep_get(dictionary, keys, default=None):
+    return reduce(
+        lambda d, key: d.get(key, default) if isinstance(d, dict) else default,
+        keys.split("."),
+        dictionary,
+    )
+
+
+def entry_not_exists(db, nested_list):
+    return deep_get(db, ".".join(nested_list)) == None
+
+
+def copy(results_json, v="all"):
+    with open(results_json) as file:
+        db = json.loads(file.read())
+        for dimension, dimension_db in db.items():
+            if entry_not_exists(new_db, [dimension]):
+                new_db[dimension] = {}
+            for domain_dim, domain_dim_db in dimension_db.items():
+                if entry_not_exists(new_db, [dimension, domain_dim]):
+                    new_db[dimension][domain_dim] = {}
+                for version, version_db in domain_dim_db.items():
+                    if v != "all" and (not (version == v or v + "_unroll" in version)):
+                        continue
+                    if entry_not_exists(new_db, [dimension, domain_dim, version]):
+                        new_db[dimension][domain_dim][version] = {}
+                    for stencil_depth, stencil_depth_db in version_db.items():
+                        new_db[dimension][domain_dim][version][stencil_depth] = db[
+                            dimension
+                        ][domain_dim][version][stencil_depth]
+
+
+copy("results/results_autotune.json", "base")
+copy("results/results_autotune_0.json", "smem")
+copy("results/results_autotune_1.json", "smem_padded")
+copy("results/results_autotune.json", "smem_register")
+
+with open("results/results_autotune.json", "w") as fp:
+    json.dump(new_db, fp)
+
+p.pprint(new_db)
